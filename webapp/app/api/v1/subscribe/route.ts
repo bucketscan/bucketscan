5c69ac57b94592758005763deadaba1f5e9ed4eb
Samuel Slade <12849513+sladesamuel@users.noreply.github.com>
2024-03-22 11:28:47 +0000

    #30 added logic for inserting into the mailinglist table

--- webapp/app/api/v1/subscribe/route.ts
+++ webapp/app/api/v1/subscribe/route.ts
@@ -1,8 +1,32 @@
+import { createSupabaseClient } from "@/utils/supabaseClient"
 import { NextRequest, NextResponse } from "next/server"
 
-export async function GET(request: NextRequest, response: NextResponse): Promise<Response> {
+const supabase = createSupabaseClient()
+
+export async function GET(request: NextRequest, _: NextResponse): Promise<Response> {
   const email = request.nextUrl.searchParams.get("email")
-  console.log(email)
+  console.log("New email submitted for mailing list", email)
+
+  // TODO: Lookup the email first so we're not trying to insert it twice.
+  const { error, count, status } = await supabase.from("mailinglist")
+    .insert({
+      email,
+      createdon: new Date().toISOString()
+    })
+    .single()
+
+  if (error) {
+    console.error(error)
+    // TODO: How do we return the error?
+  }
+
+  if (status < 200 || status > 299) {
+    console.warn("Failed to insert the email with status code", status)
+  }
+
+  if (count !== 1) {
+    console.warn("Inserted the wrong number of records", count)
+  }
 
   return new Response()
 }
