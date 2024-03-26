f582d0181e746750fd37e97529ee19038153b5d9
Samuel Slade <12849513+sladesamuel@users.noreply.github.com>
2024-03-26 20:19:42 +0000

    #30 deploy Supabase migrations in CI

--- webapp/components/GetEarlyAccess.tsx
+++ webapp/components/GetEarlyAccess.tsx
@@ -2,7 +2,10 @@
 
 import { memo, useState } from "react"
 
-const isValidEmail = (email: string): boolean => !!email // TODO: Regex validation
+const validEmailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+
+const isValidEmail = (email: string): boolean =>
+  !!email && validEmailRegex.test(email)
 
 const SubmissionSuccessful = () => (
   <section className="z-[100] mx-auto bg-gray-900 mt-10">
