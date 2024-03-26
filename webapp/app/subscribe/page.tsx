de60e0ac0d0e0b1dbe1060a4121568b4415cc028
Samuel Slade <12849513+sladesamuel@users.noreply.github.com>
2024-03-26 21:18:57 +0000

    #30 cleanup GetEarlyAccess component to make it server-side

--- webapp/app/subscribe/page.tsx
+++ webapp/app/subscribe/page.tsx
@@ -1,6 +1,11 @@
 import { memo } from "react"
 import { createSupabaseClient } from "@/utils/supabaseClient"
 
+const validEmailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+
+const isValidEmail = (email: string): boolean =>
+  !!email && validEmailRegex.test(email)
+
 const supabase = createSupabaseClient()
 
 const doesEntryExistAlready = async (email: string): Promise<boolean> => {
@@ -12,8 +17,8 @@ const doesEntryExistAlready = async (email: string): Promise<boolean> => {
   return !!existingEntry
 }
 
-const createNewEntry = async (email: string): Promise<void> => {
-  const { error, count, status } = await supabase.from("mailinglist")
+const createNewEntry = async (email: string): Promise<boolean> => {
+  const { error, status } = await supabase.from("mailinglist")
     .insert({
       email
     })
@@ -21,12 +26,17 @@ const createNewEntry = async (email: string): Promise<void> => {
 
   if (error) {
     console.error(error)
-    // TODO: How do we return the error?
+
+    return false
   }
 
   if (status < 200 || status > 299) {
-    console.warn("Failed to insert the email with status code", status)
+    console.error("Failed to insert the email with status code", status)
+
+    return false
   }
+
+  return true
 }
 
 type SearchParams = {
@@ -40,10 +50,14 @@ type Props = {
 const Page = async ({ searchParams }: Props) => {
   const email = searchParams.email as string || ""
 
-  // TODO: Handle invalid email?
+  if (!isValidEmail(email)) {
+    return <p>Email is invalid!</p>
+  }
 
   if (!await doesEntryExistAlready(email)) {
-    await createNewEntry(email)
+    if (!await createNewEntry(email)) {
+      return <p>Sorry, something went wrong!</p>
+    }
   }
 
   return (
--- webapp/components/GetEarlyAccess.tsx
+++ webapp/components/GetEarlyAccess.tsx
@@ -1,79 +1,29 @@
 import { memo } from "react"
 
-// const validEmailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+const GetEarlyAccess = () => (
+  <div className="mt-10 flex items-center justify-center gap-x-6">
+    <form
+      action="/subscribe"
+      method="get"
+    >
+      <input
+        id="email-address"
+        name="email"
+        type="email"
+        autoComplete="email"
+        required
+        className="flex-auto rounded-md border-0 px-3.5 py-2 text-black shadow-sm ring-1 ring-inset ring-white/10 transition-all focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+        placeholder="Enter your email"
+      />
 
-// const isValidEmail = (email: string): boolean =>
-//   !!email && validEmailRegex.test(email)
-
-// const SubmissionSuccessful = () => (
-//   <section className="z-[100] mx-auto bg-gray-900 mt-10">
-//     <div className="mx-auto max-w-7xl">
-//       <div className="mb-8 text-center">
-//         <h2 className="text-3xl font-semibold text-white">
-//           Thank you for joining our mailing list!
-//         </h2>
-//       </div>
-//     </div>
-//   </section>
-// )
-
-const GetEarlyAccess = () => {
-  // const [email, setEmail] = useState("")
-  // const [loading, setLoading] = useState(false)
-  // const [submitted, setSubmitted] = useState(false)
-
-  // const submit = async () => {
-  //   setLoading(true)
-
-  //   await fetch("/api/v1/subscribe?" + new URLSearchParams({
-  //     email
-  //   }))
-
-  //   setLoading(false)
-  //   setSubmitted(true)
-  // }
-
-  // if (submitted) {
-  //   return <SubmissionSuccessful />
-  // }
-
-  return (
-    <div className="mt-10 flex items-center justify-center gap-x-6">
-      <form
-        // onSubmit={e => {
-        //   e.preventDefault()
-        //   submit()
-        // }}
-        action="/subscribe"
-        method="get"
+      <button
+        type="submit"
+        className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
       >
-        <input
-          id="email-address"
-          name="email"
-          type="email"
-          autoComplete="email"
-          required
-          className="flex-auto rounded-md border-0 px-3.5 py-2 text-black shadow-sm ring-1 ring-inset ring-white/10 transition-all focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-          placeholder="Enter your email"
-        // value={email}
-        // onChange={e => setEmail(e.target.value)}
-        // disabled={loading}
-        />
-
-        <button
-          type="submit"
-          className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        // onClick={e => {
-        //   e.preventDefault()
-        //   submit()
-        // }}
-        // disabled={loading || !isValidEmail(email)}
-        >
-          Get Early Access
-        </button>
-      </form>
-    </div>
-  )
-}
+        Get Early Access
+      </button>
+    </form>
+  </div>
+)
 
 export default memo(GetEarlyAccess)
