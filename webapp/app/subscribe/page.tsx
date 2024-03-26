7c1577e7b82530ae8574cbcfbfdd7b86ec1addba
Samuel Slade <12849513+sladesamuel@users.noreply.github.com>
2024-03-26 21:25:05 +0000

    #30 subscribe page layout

--- webapp/app/subscribe/page.tsx
+++ webapp/app/subscribe/page.tsx
@@ -61,15 +61,32 @@ const Page = async ({ searchParams }: Props) => {
   }
 
   return (
-    <section className="z-[100] mx-auto bg-gray-900 mt-10">
-      <div className="mx-auto max-w-7xl">
-        <div className="mb-8 text-center">
-          <h2 className="text-3xl font-semibold text-white">
-            Thank you for joining our mailing list!
-          </h2>
+    <div className="-z-[100] bg-gray-900">
+      <div className="relative isolate px-6 pt-14 lg:px-8">
+        <div
+          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          aria-hidden="true"
+        >
+          <div
+            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+          ></div>
+        </div>
+        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+          <div className="text-center">
+            <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
+              Thank you for joining our mailing list!
+            </h1>
+            <p className="mt-6 text-lg leading-8 text-white">
+              We will be in touch once we have a launch date planned!
+            </p>
+          </div>
         </div>
       </div>
-    </section>
+    </div>
   )
 }
 
