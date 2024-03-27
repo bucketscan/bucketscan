9128abbb0ca282e29dc4ae35c9acace17aaf2649
Samuel Slade <12849513+sladesamuel@users.noreply.github.com>
2024-03-27 09:34:53 +0000

    #17 run freshclam from Lambda to download files

--- virus-api/functions/update-virus-database/index.ts
+++ virus-api/functions/update-virus-database/index.ts
@@ -1,3 +1,28 @@
+import { ExecException, exec } from "child_process"
+
+const runFreshClam = async (): Promise<void> => {
+  console.log("Running freshclam to download virus database files...")
+
+  return new Promise<void>((resolve, reject) => {
+    exec("/bin/freshclam", (error: ExecException, stdout: string, stderr: string) => {
+      console.log("Output", stdout)
+
+      if (stderr) console.warn(stderr)
+
+      if (error) {
+        console.error(error)
+
+        reject()
+      }
+      else {
+        resolve()
+      }
+    })
+  })
+}
+
 export default async (): Promise<void> => {
-  console.log("Hello, world!")
+  await runFreshClam()
+
+  console.log("TODO: Copy database files into S3 bucket")
 }
