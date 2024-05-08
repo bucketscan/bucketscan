aa2b7b17816e941e6468a5d21fefc73b08ad4deb
Josh Ghent <me@joshghent.com>
2024-05-09 00:01:10 +0100

    feat: add crisp chat widget

--- webapp/app/layout.tsx
+++ webapp/app/layout.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import Layout from "@/components/Layout";
 import { Providers } from "./providers";
 import type { Metadata } from "next";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Bucketscan",
@@ -16,12 +17,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <script defer src="https://analytics.eu.umami.is/script.js" data-website-id="2a210cba-0a3f-4d08-851e-51cfd9a47fb3"></script>
+        <script
+          defer
+          src="https://analytics.eu.umami.is/script.js"
+          data-website-id="2a210cba-0a3f-4d08-851e-51cfd9a47fb3"
+        ></script>
       </head>
       <body>
         <Providers>
           <Layout>{children}</Layout>
         </Providers>
+        <Script id="crisp-chat" strategy="afterInteractive">
+          {`window.$crisp=[];window.CRISP_WEBSITE_ID="aeec63d4-3cb6-4e20-b0a4-76cfabe7a4f6";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();`}
+        </Script>
       </body>
     </html>
   );
