4510a5f77eab7542b0094479ad09bfed9eedf4f3
Samuel Slade <12849513+sladesamuel@users.noreply.github.com>
2024-06-26 22:34:36 +0100

    Corrected test/variable in condition

--- virus-api/terraform/guardduty.tf
+++ virus-api/terraform/guardduty.tf
@@ -33,8 +33,8 @@ data "aws_iam_policy_document" "guardduty_permissions" {
     ]
 
     condition {
-      variable = "StringLike"
-      test     = "events:ManagedBy"
+      test     = "StringLike"
+      variable = "events:ManagedBy"
       values   = ["malware-protection-plan.guardduty.amazonaws.com"]
     }
   }
