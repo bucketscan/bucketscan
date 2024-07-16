aac4b3e096a3195f9bc2c17d5292e46f66683225
Samuel Slade <12849513+sladesamuel@users.noreply.github.com>
2024-07-16 22:16:03 +0100

    Step Function now triggered when file is scanned

--- virus-api/terraform/handle-scanned-files.tf
+++ virus-api/terraform/handle-scanned-files.tf
@@ -1,4 +1,56 @@
 #################################################################################
+# Event Rule
+#################################################################################
+resource "aws_cloudwatch_event_rule" "scan_results" {
+  name        = "bucketscan-guardduty-malware-scan-result"
+  description = "Handle scan results from GuardDuty Malware Protection for the S3 bucket: bucketscan-scan-files"
+
+  event_pattern = jsonencode({
+    detail-type = ["GuardDuty Malware Protection Object Scan Result"],
+    source      = ["aws.guardduty"]
+  })
+}
+
+data "aws_iam_policy_document" "trigger_handle_scanned_files_workflow" {
+  version = "2012-10-17"
+
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["events.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "trigger_handle_scanned_files_workflow" {
+  name               = "bucketscan-trigger-handle-scanned-files-workflow"
+  assume_role_policy = data.aws_iam_policy_document.trigger_handle_scanned_files_workflow.json
+}
+
+data "aws_iam_policy_document" "trigger_handle_scanned_files_workflow_permission" {
+  version = "2012-10-17"
+
+  statement {
+    actions   = ["states:StartExecution"]
+    resources = [aws_sfn_state_machine.handle_scanned_files.arn]
+  }
+}
+
+resource "aws_iam_role_policy" "trigger_handle_scanned_files_workflow_permission" {
+  name   = "bucketscan-trigger-handle-scanned-files-workflow"
+  role   = aws_iam_role.trigger_handle_scanned_files_workflow.id
+  policy = data.aws_iam_policy_document.trigger_handle_scanned_files_workflow_permission.json
+}
+
+resource "aws_cloudwatch_event_target" "handle_scanned_files" {
+  rule     = aws_cloudwatch_event_rule.scan_results.name
+  arn      = aws_sfn_state_machine.handle_scanned_files.arn
+  role_arn = aws_iam_role.trigger_handle_scanned_files_workflow.arn
+}
+
+#################################################################################
 # IAM Role
 #################################################################################
 data "aws_iam_policy_document" "handle_scanned_files" {
