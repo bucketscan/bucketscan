b699cf0fe90d83d53ec177888edb9dcc00ccc916
Samuel Slade <12849513+sladesamuel@users.noreply.github.com>
2024-07-30 21:09:48 +0100

    Implemented the get scan by Id route (#42)
    * Implemented the get scan by Id route
    
    * Removed unused upload route

---
+++ webapp/app/api/v1/scans/[scanId]/route.ts
@@ -0,0 +1,53 @@
+import { NextRequest, NextResponse } from "next/server"
+import { badRequest, HttpResponse, internalServerError, notFound, ok } from "@/app/api/responses"
+import getAccountId from "@/app/api/getAccountId"
+import { isError } from "@/app/api/Result"
+import { createSupabaseClient } from "@/utils/supabaseClient"
+
+const supabase = createSupabaseClient()
+
+type ScanResult = "pending" | "complete_clean" | "complete_infected" | "failed"
+
+type GetScanByIdRequest = {
+  params: {
+    scanId: string
+  }
+}
+
+type GetScanByIdResult = {
+  scanId: string
+  result: ScanResult
+}
+
+export async function GET(request: NextRequest, { params }: GetScanByIdRequest): Promise<NextResponse<HttpResponse | GetScanByIdResult>> {
+
+  const accountId = getAccountId(request)
+  if (isError(accountId)) {
+    return badRequest(accountId.message)
+  }
+
+  const { scanId } = params
+  if (!scanId) {
+    return badRequest("Missing scanId from URL")
+  }
+
+  const { data, error } = await supabase
+    .from('scans')
+    .select()
+    .eq("id", scanId)
+    .single()
+
+  if (error) {
+    console.error(JSON.stringify(error))
+    return internalServerError(error.message)
+  }
+
+  if (!data) {
+    return notFound(`No scan record was found with Id ${scanId}`)
+  }
+
+  return ok("OK", {
+    scanId,
+    scanResult: data.result
+  })
+}
