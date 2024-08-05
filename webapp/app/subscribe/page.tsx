44d15c588155b319b26071ed36addb09817d1865
Josh Ghent <me@joshghent.com>
2024-08-05 10:30:28 +0100

    fix: updated references to supabaseclient

--- webapp/app/subscribe/page.tsx
+++ webapp/app/subscribe/page.tsx
@@ -1,62 +1,65 @@
-import { memo } from "react"
-import { createSupabaseClient } from "@/utils/supabaseClient"
+import { memo } from "react";
+import { createClient } from "@/utils/supabase/client";
 
-const validEmailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+const validEmailRegex =
+  /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
 const isValidEmail = (email: string): boolean =>
-  !!email && validEmailRegex.test(email)
+  !!email && validEmailRegex.test(email);
 
-const supabase = createSupabaseClient()
+const supabase = createClient();
 
 const doesEntryExistAlready = async (email: string): Promise<boolean> => {
-  const { data: existingEntry } = await supabase.from("mailinglist")
+  const { data: existingEntry } = await supabase
+    .from("mailinglist")
     .select()
     .filter("email", "eq", email)
-    .single()
+    .single();
 
-  return !!existingEntry
-}
+  return !!existingEntry;
+};
 
 const createNewEntry = async (email: string): Promise<boolean> => {
-  const { error, status } = await supabase.from("mailinglist")
+  const { error, status } = await supabase
+    .from("mailinglist")
     .insert({
-      email
+      email,
     })
-    .single()
+    .single();
 
   if (error) {
-    console.error(error)
+    console.error(error);
 
-    return false
+    return false;
   }
 
   if (status < 200 || status > 299) {
-    console.error("Failed to insert the email with status code", status)
+    console.error("Failed to insert the email with status code", status);
 
-    return false
+    return false;
   }
 
-  return true
-}
+  return true;
+};
 
 type SearchParams = {
-  [key: string]: string | string[] | undefined
-}
+  [key: string]: string | string[] | undefined;
+};
 
 type Props = {
-  searchParams: SearchParams
-}
+  searchParams: SearchParams;
+};
 
 const Page = async ({ searchParams }: Props) => {
-  const email = searchParams.email as string || ""
+  const email = (searchParams.email as string) || "";
 
   if (!isValidEmail(email)) {
-    return <p>Email is invalid!</p>
+    return <p>Email is invalid!</p>;
   }
 
-  if (!await doesEntryExistAlready(email)) {
-    if (!await createNewEntry(email)) {
-      return <p>Sorry, something went wrong!</p>
+  if (!(await doesEntryExistAlready(email))) {
+    if (!(await createNewEntry(email))) {
+      return <p>Sorry, something went wrong!</p>;
     }
   }
 
@@ -87,7 +90,7 @@ const Page = async ({ searchParams }: Props) => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default memo(Page)
+export default memo(Page);
